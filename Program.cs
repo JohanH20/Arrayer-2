4f5c22d68a4810d7748aabf1ab9d5bfc64e23387
JohanH20 <54435441+JohanH20@users.noreply.github.com>
2019-10-04 09:41:38 +0200

    Gjorde klart uppgift 2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrayer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+            double temp = Math.Round(Medelvärde(temperatur),1);
+            Console.WriteLine("Medelvärdet är:" + temp);
+        }
+        static double Medelvärde(double[] t)
+        {
+            double summa = 0;
+            for(int i = 0; i < t.GetLength(0); i++)
+                summa += t[i];
+            return (summa / t.Length);
+        }
+    }
+}
